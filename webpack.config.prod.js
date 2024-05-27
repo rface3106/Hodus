dce898f6c4b082fb1b3565965ea9363213dce11f
rface3106 <rface3106@gmail.com>
2024-05-27 10:38:48 +0900

    파일 정리,index 수정

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
   mode: 'production',
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: './dududu.html',
     }),
     new CopyPlugin({
       patterns: [
